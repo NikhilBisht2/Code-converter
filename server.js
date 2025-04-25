ad5fb0cbe82de42098fd482a4338c14a1b9251bc
NikhilBisht2 <139066696+NikhilBisht2@users.noreply.github.com>
2025-04-25 16:27:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ ${wrapCodeIfNeeded(code, sourceLanguage)}
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
 
